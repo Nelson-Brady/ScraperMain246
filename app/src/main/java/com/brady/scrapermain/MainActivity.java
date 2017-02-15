41920046b6bc76c0afc2ee10328e7cb8b890ad70
spencer <pec15002@byui.edu>
2017-02-15 09:27:16 -0700

    added comment

--- app/src/main/java/com/brady/scrapermain/MainActivity.java
+++ app/src/main/java/com/brady/scrapermain/MainActivity.java
@@ -2,7 +2,7 @@ package com.brady.scrapermain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//The main Activity is great! is fun stuff
+//The main Activity is great! is fun stuff yo
 public class MainActivity extends AppCompatActivity {
 
     @Override
