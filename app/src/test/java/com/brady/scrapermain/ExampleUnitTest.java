03df465113b6d9a8f74e2b3edca5617350dfc184
Ryan Walker <RyanPrestonWalker@gmail.com>
2017-02-22 09:48:36 -0700

    Test functions work and are available to be edited

--- app/src/test/java/com/brady/scrapermain/ExampleUnitTest.java
+++ app/src/test/java/com/brady/scrapermain/ExampleUnitTest.java
@@ -14,4 +14,16 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    /*@Test
+    public void AJAX_isCorrect() throws Exception {
+    }
+
+    @Test
+    public void loadFile_isCorrect() throws Exception {
+    }
+
+    @Test
+    public void Intent_isCorrect() throws Exception {
+    }*/
 }
