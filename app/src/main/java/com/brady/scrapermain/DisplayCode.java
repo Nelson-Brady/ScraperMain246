e76ed0fce401d65f3fd4f59bb9d0ca54b1e241f0
spencer <pec15002@byui.edu>
2017-02-24 09:57:54 -0700

    Merge branch 'master' of https://github.com/Nelson-Brady/ScraperMain246
    # Conflicts:
    #	app/src/main/java/com/brady/scrapermain/MainActivity.java

---
+++ app/src/main/java/com/brady/scrapermain/DisplayCode.java
@@ -0,0 +1,27 @@
+package com.brady.scrapermain;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DisplayCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_code);
+
+        Intent intent = getIntent();
+
+        String since = intent.getStringExtra("Since");
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(30);
+        textView.setText(since);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_code);
+        layout.addView(textView);
+    }
+}
--- app/src/main/java/com/brady/scrapermain/inputcode.java
+++ app/src/main/java/com/brady/scrapermain/inputcode.java
@@ -1,11 +1,20 @@
 package com.brady.scrapermain;
 
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputFilter;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
 
 public class inputcode extends AppCompatActivity {
+
+    private String SinceTime;
+    private String untilTime;
+    private String GeosAddress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +22,30 @@ public class inputcode extends AppCompatActivity {
         setContentView(R.layout.activity_inputcode);
         setTitle("New Input");
         // create object editText
-        EditText editText = (EditText) findViewById(R.id.editText);
-        //This function insures that all characters are uppercase
-        editText.setFilters(new InputFilter[] {new InputFilter.AllCaps()});
+        EditText Geos = (EditText) findViewById(R.id.editText);
 
+        //This function insures that all characters are uppercase
+        Geos.setFilters(new InputFilter[] {new InputFilter.AllCaps()});
     }
+// This function validates the form when search is pressed
+
+    void checkForm(View view) {
+
+        EditText Since = (EditText) findViewById(R.id.editText3);
+        EditText until = (EditText) findViewById(R.id.editText2);
+        EditText Geos = (EditText) findViewById(R.id.editText);
+
+        SinceTime = Since.getText().toString();
+        untilTime = until.getText().toString();
+        GeosAddress = Geos.getText().toString();
+
+        Intent intent = new Intent(this, DisplayCode.class);
+
+        intent.putExtra("Since", SinceTime);
+        intent.putExtra("until", untilTime);
+        intent.putExtra("geos", GeosAddress);
+
+        startActivity(intent);
+        }
+
 }
