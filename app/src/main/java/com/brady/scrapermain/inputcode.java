b410ff58ecefe095d1cac6edad99966f323c8caf
Nelson-Brady <alexisfire@gmail.com>
2017-03-29 09:22:42 -0600

    Brady - Decoding data

--- app/src/main/java/com/brady/scrapermain/inputcode.java
+++ app/src/main/java/com/brady/scrapermain/inputcode.java
@@ -66,6 +66,89 @@ public class inputcode extends AppCompatActivity {
         //((Activity)context).finish();
     }
 
+
+    //This string assumes user has removed HTML for us
+    //Also needs user to give us an integer for data size
+
+    String Test = "DD23122617085012451G46+1NN175EXE00087`BCT@Fd@Fc@Fb@Fa@Fc@F`@F_@F^@F]@F]@F[@F[@Fm@Fl@Fk@Fj@Fi@Fh@Fh@Fg@Ff@Fe@Fd@Fc@T@@T@@TJj ";
+
+
+    String headerOnly = Test.substring(0, 41);
+    String dataOnly = Test.substring(41);
+
+    //Display tests
+        System.out.println("headerOnly: " + headerOnly);
+        System.out.println("dataOnly: " + dataOnly);
+
+
+    //Puts each piece of data in array
+    //Length / dataBits should be count  This isn't quite right yet, due to battery level at end.
+    int dataBits = 3;
+    int count = dataOnly.length() / dataBits;
+    String[] data = new String[count];
+    int i;
+    int ss = 0;
+        for (i = 0; i < count; i++) {
+        data[i] = dataOnly.substring(ss, ss + dataBits);
+        ss+=dataBits;
+    }
+
+
+    //Decode the data
+    int sumData = 0;
+    int index = dataBits;
+        for (i=0; i<count; i++){
+        for (int j = 0; j <dataBits; j++) {
+            char s = data[i].charAt(j);
+            index--;
+
+            int newValue = solveNum(s, index);
+            sumData += newValue;
+            //System.out.println("new: " + newValue);
+            //System.out.println("sum: " + sumData);
+        }
+        System.out.println("sum: " + sumData);
+        sumData = 0;
+        index = dataBits;
+    }
+
+    //Print out battery level
+    int length = dataOnly.length();
+    String battery = dataOnly.substring(length - 2);
+    char b = battery.charAt(0);
+    float bat = solveNum(b, 0);
+    bat = (float) (bat * .3124 + 0.311);
+        System.out.println("Battery: " + bat);
+
+}
+
+    private static int solveNum(char s, int index) {
+        //How to decode each piece of data???
+
+        //int ascii = s.charAt(0);
+        int ascii = s;
+        //int newAsc = (ascii & 0x3f);
+
+        int value = 0;
+        if (index == 0){
+            value = (ascii & 0x3f);
+        }
+        else if (index == 1) {
+            value = (ascii & 0x3f);
+            value *= 64;
+        }
+        else if (index == 2) {
+            value = (ascii & 0x3f);
+            value *= 4096;
+        }
+        else {
+            value = (ascii & 0x3f);
+            value *= 262144;
+
+        }
+
+        return value;
+    }
     /*
     // this function parses the header information
     void parseInfo(String data) {
